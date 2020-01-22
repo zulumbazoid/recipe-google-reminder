f186c11ccf79b08ff184c09c9971ef1abd50e468
zulumbazoid <51781401+zulumbazoid@users.noreply.github.com>
2020-01-22 23:02:15 +0100

    v1.1.2

--- webview.js
+++ webview.js
@@ -1,2 +1,6 @@
 'use strict'
+
+module.exports = Franz => {
+    Franz.injectCSS(path.join(__dirname, 'darkmode.css'));
+};
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJwb2NrZXQvd2Vidmlldy5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
